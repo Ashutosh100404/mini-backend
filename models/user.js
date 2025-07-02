1cc610f0781880189c7dcc4d54fe4b7d7a207eb6
Ashutosh100404 <divusingh1004@gmail.com>
2025-07-02 23:48:06 +0530

    new changes

--- models/user.js
+++ models/user.js
@@ -1,16 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
+mongoose.connect(`mongodb://127.0.0.1:27017/miniPro`);
 
 const userSchema = mongoose.Schema({
-    username: String,
-    name: String,
-    age: Number,
-    email: String,
-    passwrod: String,
-    posts: [{
-        type:mongoose.Schema.Types.ObjectId, ref: "post"
-    }]
-})
+  username: String,
+  name: String,
+  age: Number,
+  email: String,
+  password: String,
+  profilepic: {
+    type: String,
+    default: "default.jpg",
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post",
+    },
+  ],
+});
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model("user", userSchema);
